848a0d6c4518ce66340014b910d711530f324b98
Elizabeth Glenn Guy <elizg23@yahoo.com>
2019-01-30 12:53:56 -0800

    add functions for extracting data and making dataframe of gender and results

--- ult_sign_scrape/UltraSignup.py
+++ ult_sign_scrape/UltraSignup.py
@@ -14,16 +14,20 @@ import plotly.graph_objs as go
 root_url = 'http://ultrasignup.com'
 #index_url = root_url + '/results_event.aspx?did=41880' #add index for event of interest
 
-browser = Chrome()
-browser.get(index_url)
 
-sel = "gbox_list" 
-cascade100results=browser.find_element_by_id(sel) #setting a css selector
-table_rows = cascade100results.text.split('\n')
-
-runner_rows = [row.split() for row in table_rows]
-cols = runner_rows[0:10] #column headers
-content = runner_rows[11:] #the actual runner results
+def get_results(index_url):
+    '''Starts web scraper to get table
+    with runner results.  Returns column headers
+    and result data.'''
+    browser = Chrome()
+    browser.get(index_url)
+    sel = "gbox_list" 
+    cascade100results=browser.find_element_by_id(sel) #setting a css selector
+    table_rows = cascade100results.text.split('\n')
+    runner_rows = [row.split() for row in table_rows]
+    cols = runner_rows[0:10] #column headers
+    content = runner_rows[11:] #the actual runner results
+    return (cols, content)
 
 #find index of Did Not Finish and Did Not Start
 def find_idx(content):
@@ -49,4 +53,4 @@ def make_finisher_df(content, idx_1):
     df['Time'] = cascade1002017['Time'].str.split(':')
     df['Time'] = cascade1002017['Time'].apply(lambda x: int(x[0]) * 60 + int(x[1]) + float(x[2])/60)
     return df
-    
+
